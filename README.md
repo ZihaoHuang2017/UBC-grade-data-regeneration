33dc0acb5a6b50d7adc442f15d223a42381ab30e
Zihao Huang <zihaohuang2017@gmail.com>
2023-06-11 12:14:08 -0700

    Use d'Hondt allocation

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # UBC-grade-data-regeneration
-In the year of 2023, UBC PAIR stopped giving out the entirety of score distribution nor the standard deviation (The letter grades with fewer than 6 entries are removed). This has made the analysis on prior results much harder. Fortunately the score of the lower and higher quantile are given, so assuming a mostly normal distribution one could figure out the relative proportions of each letter grade, and multiply them to achieve an estimate of the persons that are in that grade bracket. 
+In the year of 2023, UBC PAIR stopped giving out the entirety of score distribution nor the standard deviation (The letter grades with fewer than 6 entries are removed). This has made the analysis on prior results much harder. Fortunately the score of the lower and higher quantile are given, so assuming a mostly normal distribution one could figure out the relative proportions of each letter grade and allocate a number of students to the proportions. 
 
 This tool will work the best if there's only 1 or 2 fields missing or if the lowest/highest score are somewhat close. I do not guarantee that the generated result will be accurate at all.
 
@@ -8,16 +8,35 @@ This tool doesn't consider the possiblities of:
 - Bimodal Distribution (Common in courses like CPSC 110, where you fail the entire course if you fail the combined average of exams)
 - Skewed results (Happens when the presumed mean is quite different from the actual average)
 - Pure chaos (MATH 320)
+- Too many dropped students/grade entry error (2022W MATH 100 1A1 - either 154 students dropped out or the grades are incorrectly inputed)
 
 ## How to use it
 You will need to have Python configured.
 1. Fill INITIAL_ARR with the existing score distribution, put 0s if data is not found.
-2. Fill LO, HI according to the lowest/highest score.
-3. Fill QUANTILE_25, QUANTILE-75 according to the 25th/75th percentile score.
-4. Fill TOTAL_POP according to the Reported section.
-5. Run the file. The newly generated list represents the most possible scenario.
+2. Fill QUANTILE_25, QUANTILE_75, HI, LO, TOTAL_POP according to the stats provided.
+3. Run the file. The newly generated list represents the most possible scenario according to D'Hondt method of allocation.
 
+## Examples
+Fields that are *italicized* are generated; Fields that are **bolded** are generated results that contain error and should be adjusted.
+
+2022W CPSC 110 101:
+
+[39, *0*, *1*, *3*, **6**, 6, 15, 25, 39, 59, 70]
+
+2022W WRDS 150B 510:
+
+[*1*, *0*, *0*, *1*, *2*, *3*, 10, 6, *5*, 0, 0]
+
+2022W SCIE 113 108:
+
+[*1*, *0*, *0*, *0*, *0*, *0*, *0*, *2*, *4*, 7, 11]
+
+2022W JAPN 320 003 (No information given):
+
+[*0*, *0*, *0*, *1*, *1*, *1*, *2*, *2*, *2*, *2*, *2*]
+
+2022W JAPN 320 003 (Provided that there's only 1 A+ student):
+
+[*0*, *0*, *0*, *1*, *1*, *1*, *2*, *2*, *3*, *2*, 1]
 ## Future potential expansions
 - Use .csv data directly
-- Round the numbers to be integers (D'Hondt or Huntington-Hill?)
-- Make some of the logics run slightly faster
--- regen.py
+++ regen.py
@@ -14,39 +14,41 @@ ERFINV_25 = 0.674489750196  # Equal to erf^(-1)(0.25)
 
 
 def get_table_index(score: int) -> int:
-    return bisect_left(NORMAL_SCORE_TABLE, score)
+    # Returns where the score lies in the grade range.
+    return bisect_left(NORMAL_SCORE_TABLE, score + 1)
 
 
-def get_custom_capped_percentage(mean, stdev, lo, hi):
+def get_custom_capped_percentage(score, mean, stdev, lo, hi):
+    # Returns the percentage of the student as a function.
     def normal_pdf(x):
-        return 1 / 2 * (1 + math.erf((x - mean) / (math.sqrt(2) * stdev)))
+        return (1 + math.erf((x - mean) / (math.sqrt(2) * stdev))) / 2
 
-    def percentage_calc(x):
-        if x == 100:
-            return 1
-        return (normal_pdf(x) - normal_pdf(lo)) / (normal_pdf(hi) - normal_pdf(lo))
-
-    return percentage_calc
+    if score == 100:
+        return 1
+    return (normal_pdf(score) - normal_pdf(lo)) / (normal_pdf(hi) - normal_pdf(lo))
 
 
 def get_stdev(quantile_25, quantile_75):
+    # Returns the persumed stdev since each quantile is approximately 0.675 stdevs
+    # apart from the mean.
     return (quantile_75 - quantile_25) / (2 * ERFINV_25)
 
 
 def get_to_be_filled_entries(arr, lo, hi) -> list[int]:
+    # Returns a list of to-be-filled indexes.
     lo_index = get_table_index(lo)
     hi_index = get_table_index(hi)
-    result = [0] * 11
+    result = []
     for i in range(lo_index, hi_index + 1):
         if arr[i] == 0:
-            result[i] = 1
+            result.append(i)
     return result
 
 
 def get_range(index, lo, hi) -> tuple[int, int]:
     if index == 0:
         return lo, NORMAL_SCORE_TABLE[index]
-    if index == 10:
+    if index >= 10:
         return max(NORMAL_SCORE_TABLE[index], lo), min(100, hi)
     else:
         return max(NORMAL_SCORE_TABLE[index], lo), min(
@@ -54,37 +56,50 @@ def get_range(index, lo, hi) -> tuple[int, int]:
         )
 
 
+def dhont(num_seats: int, votes: dict, lo, hi):
+    # Returns the allocation of student according to d'Hondt algorithm,
+    # but the lowest and highest entry are guaranteed a seat
+    def preprocess(t_votes: dict, seats: dict):
+        # Since there's guaranteed to be an entry for lo and hi bracket
+        if get_table_index(lo) in seats:
+            seats[get_table_index(lo)] += 1
+            t_votes[get_table_index(lo)] = votes[get_table_index(lo)] / 2
+        if get_table_index(hi) in seats:
+            seats[get_table_index(hi)] += 1
+            t_votes[get_table_index(hi)] = votes[get_table_index(hi)] / 2
+
+    t_votes = copy.deepcopy(votes)
+    seats = {}
+    for key in votes:
+        seats[key] = 0
+    preprocess(t_votes, seats)
+    while sum(seats.values()) < num_seats:
+        next_seat = max(t_votes, key=t_votes.get)
+        seats[next_seat] += 1
+        t_votes[next_seat] = votes[next_seat] / (seats[next_seat] + 1)
+    return seats
+
+
 def fill_arr(arr, lo, hi, quantile_25, quantile_75, total_pop):
     result = copy.deepcopy(arr)
     to_be_filled_entries = get_to_be_filled_entries(arr, lo, hi)
-    unfilled_relative_percentage = [0] * 11
     stdev = get_stdev(quantile_25, quantile_75)
     mean = (quantile_25 + quantile_75) / 2
     to_be_filled_pop = total_pop - sum(arr)
-    print(to_be_filled_pop, mean, stdev)
-    for index, item in enumerate(to_be_filled_entries):
-        if item:
-            range_lo, range_hi = get_range(index, lo, hi)
-            unfilled_relative_percentage[index] = get_custom_capped_percentage(
-                mean, stdev, lo, hi
-            )(range_hi) - get_custom_capped_percentage(mean, stdev, lo, hi)(range_lo)
-    
-    total_weight_unfilled = sum(unfilled_relative_percentage)
-    assert total_weight_unfilled <= 1
-    for index, item in enumerate(unfilled_relative_percentage):
-        if not result[index]:
-            result[index] = item * to_be_filled_pop/total_weight_unfilled
-        
+    alloc_dict = {}
+    for i in to_be_filled_entries:
+        range_lo, range_hi = get_range(i, lo, hi)
+        alloc_dict[i] = get_custom_capped_percentage(range_hi, mean, stdev, lo, hi)
+        -get_custom_capped_percentage(range_lo, mean, stdev, lo, hi)
+    dhont_result = dhont(to_be_filled_pop, alloc_dict, lo, hi)
+    for key in dhont_result:
+        result[key] = dhont_result[key]
     assert sum(result) == total_pop
-    partial_sum = 0
-    for i in result:
-        if not isinstance(i, int):
-            partial_sum += i
-    assert abs(partial_sum - to_be_filled_pop) < 0.1
     return result
 
-INITIAL_ARR = [39, 0, 0, 0, 0, 6, 15, 25, 39, 59, 70]
-LO, HI = 6, 98
-QUANTILE_25, QUANTILE_75 = 75, 90
-TOTAL_POP = 263
+
+# [39, 0, 0, 0, 0, 6, 15, 25, 39, 59, 70]
+# 75, 90, 98, 6, 263
+INITIAL_ARR = [0] * 10 + [1]
+QUANTILE_25, QUANTILE_75, HI, LO, TOTAL_POP = 69, 83, 90, 60, 13
 print(fill_arr(INITIAL_ARR, LO, HI, QUANTILE_25, QUANTILE_75, TOTAL_POP))
